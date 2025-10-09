c5d45639131a04b02551d59071b7ee4d208d2ad2
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-10-09 14:46:10 +0530

    init commit

---
+++ src/Recursion/Q4_SumOfN.java
@@ -0,0 +1,14 @@
+public class Q4_SumOfN {
+    public static void main(String[] args) {
+        System.out.println( factorial(5));
+    };
+
+    static int factorial(int n)
+    {
+        if(n==0){
+            return 1;
+        }
+
+        return n+factorial(n-1);
+    }
+}
---
+++ src/Recursion/Q5_sumOfdigit.java
@@ -0,0 +1,15 @@
+public class Q5_sumOfdigit {
+    public static void main(String[] args) {
+        System.out.println( sumDigit(1342));
+    };
+
+    static int sumDigit(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+int lDigit=n%10;
+        return lDigit+sumDigit(n/10);
+    }
+}
