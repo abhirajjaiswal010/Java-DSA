e7c716414b224bc029cca9f1baee3cd67c79faac
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-10-13 17:18:29 +0530

    init commit

---
+++ src/Recursion/Q6_productOfDigit.java
@@ -0,0 +1,17 @@
+public class Q6_productOfDigit {
+    public static void main(String[] args) {
+        System.out.println( productDigit(1342));
+    };
+
+    static int productDigit(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        int lDigit=n%10;
+        return lDigit*productDigit(n/10);
+    }
+
+
+}
