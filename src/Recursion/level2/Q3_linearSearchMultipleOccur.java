af2e03d734c825270358ea17654ae4309a3de704
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-10-16 16:17:37 +0530

    init commit

--- src/Recursion/level2/Q3_linearSearchMultipleOccur.java
+++ src/Recursion/level2/Q3_linearSearchMultipleOccur.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Q3_linearSearchMultipleOccur {
 
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,4,6,5};
-        check(arr,0,4);
+        int[] arr={1,2,2,4};
+        check(arr,0,2);
         System.out.println( list);
     }
 static ArrayList<Integer> list= new ArrayList<>();
     static void check(int[] arr,int index,int target){
-        if(index==arr.length-1)
+        if(index==arr.length)
             return;
         if(target==arr[index])
             list.add(index);
