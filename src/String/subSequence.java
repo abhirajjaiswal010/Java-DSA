56c8b97f83464f8a5658983f1541a478e4b4abd0
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-11-08 13:12:11 +0530

    init commit

---
+++ src/String/subSequence.java
@@ -0,0 +1,30 @@
+package String;
+
+public class subSequence {
+
+    public static void main(String[] args) {
+        // Start me function call kar rahe hain
+        // "" → abhi tak kuch bhi select nahi kiya
+        // "abc" → ye string ka subsequence banana hai
+        subseq("", "abc");
+    }
+
+    static void subseq(String p , String up){
+        // Jab unprocessed string khatam ho jaaye
+        if(up.isEmpty()){
+            // Agar processed string khali hai to ye empty subset hai
+            if(p.isEmpty()) System.out.println("null");
+            else System.out.println(p);
+            return;
+        }
+
+        // Pehla character nikal lo
+        char ch = up.charAt(0);
+
+        // Character ko include karo
+        subseq(p + ch, up.substring(1));
+
+        // Character ko exclude karo
+        subseq(p, up.substring(1));
+    }
+}
